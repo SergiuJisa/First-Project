b6c1fec3213de452ccae707059868d109bb7d2a6
Sergiu Jisa <vasile_sergiu_jisa@yahoo.com>
2019-12-18 13:25:59 +0200

    First commit

---
+++ src/com/firstLesson/ITeaHome.java
@@ -0,0 +1,10 @@
+package com.firstLesson;
+
+public class ITeaHome {
+
+    public static void main(String[] args) {
+	System.out.println("Salut Andrei!");
+	System.out.println("Welkome to ITea Home Academy, Sergiu :)");
+    }
+}
+
